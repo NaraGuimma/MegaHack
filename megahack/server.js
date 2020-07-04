939db2768f464afe5bf279333bcd2b539d3eda26
NaraGuimma <naraguimma@gmail.com>
2020-07-04 17:01:43 -0300

    arrumando o server.js e package.json

--- megahack/server.js
+++ megahack/server.js
@@ -1,15 +1,14 @@
 //Importa as dependências que acabamos de instalar
 const express = require("express");
 const path = require("path");
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/NOME_DO_SEU_PROJETO"));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/NOME_DO_SEU_PROJETO/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
