eac8332231f7076351979bfaa9ace93110d360f8
flavio <flavio.diminuto@outlook.com>
2020-07-04 20:14:53 -0300

    Ajuste app.module 01 (add RouteModule)

--- megahack/src/app/app.module.ts
+++ megahack/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule }  from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
     BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([ 
+      //adicionar as rotas aqui
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
